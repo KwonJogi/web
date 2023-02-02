dfbd779c989bdb1f5b8ae48ca1f6baff71c73779
jeongin <kwjddls1919@naver.com>
2023-02-02 17:41:12 +0900

    day04

---
+++ day04/out.js
@@ -0,0 +1,13 @@
+function wash() {
+  console.log("1.먼지를 턴다.");
+  console.log("2. 물에 집어 넣는다.");
+  console.log("3. 비누를 바른다.");
+  console.log("4. 주무른다.");
+  console.log("5. 건조한다.");
+}
+
+function clean() {
+  console.log("1. 청소기 돌린다.");
+  console.log("2. 물청소 한다.");
+  console.log("3. 책상 정리한다.");
+}
---
+++ day04/out2.js
@@ -0,0 +1,11 @@
+function 여행하다() {
+  console.log("1. 숙소를 예약한다.");
+  console.log("2. 짐을 싼다.");
+  console.log("3. 이동한다.");
+  console.log("4. 즐긴다.");
+}
+
+function 쉬다() {
+  console.log("1. 침대에 눕는다.");
+  console.log("2. 유튜브를 본다.");
+}
